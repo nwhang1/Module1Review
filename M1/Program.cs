f7be73eb456839051a1d60bea69bc65c02390417
nwhang1 <nicholasjwhang@gmail.com>
2024-03-20 13:11:19 -0700

    Formatted for PascalCasing

--- M1/Program.cs
+++ M1/Program.cs
@@ -13,7 +13,7 @@ namespace M1
             
             // Utilizing the double method, which allows for decimals to be used and returned
             double rad = double.Parse(Console.ReadLine());
-            double circleArea = cir(rad);
+            double circleArea = Cir(rad);
 
             Console.WriteLine($"The area of this circle is {circleArea}");
 
@@ -21,7 +21,7 @@ namespace M1
             Console.WriteLine("Let's calculate the area of a Triangle, what is the base length and the height?");
             double baseLength = double.Parse(Console.ReadLine());
             double height = double.Parse(Console.ReadLine());
-            double triArea = tri(baseLength, height);
+            double triArea = Tri(baseLength, height);
 
             Console.WriteLine($"The area of this triangle is {triArea}");
 
@@ -29,32 +29,32 @@ namespace M1
             Console.WriteLine("Let's calculate the area of a Rectangle, what is the length and the width?");
             double length = double.Parse(Console.ReadLine());
             double width = double.Parse(Console.ReadLine());
-            double rectArea = rect(length, width);
+            double rectArea = Rect(length, width);
 
             Console.WriteLine($"The area of this rectangle is {rectArea}");
             //Square Method
             Console.WriteLine("Let's calculate the area of a Square, what is the length of one side?");
             double side = double.Parse(Console.ReadLine());
-            double sqArea = sq(side);
+            double sqArea = Sq(side);
 
             Console.WriteLine($"The area of this square is {sqArea}");
         }
-        static double cir(double rad)
+        static double Cir(double rad)
         {
             return rad * rad * 3.14;
         }
 
-        static double tri(double baseLength, double height)
+        static double Tri(double baseLength, double height)
         {
             return baseLength * height * .5;
         }
 
-        static double rect(double length, double width)
+        static double Rect(double length, double width)
         {
             return length * width;
         }
 
-        static double sq(double side)
+        static double Sq(double side)
         {
             return side * side;
         }
